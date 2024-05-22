6941469307b628debfa8ac5779edc5a99f8cff03
Toshiaki Maki <makingx@gmail.com>
2024-05-23 00:56:14 +0900

    Filter requests from Amazon-Route53-Health-Check-Service

--- src/main/java/com/example/hello/AppConfig.java
+++ src/main/java/com/example/hello/AppConfig.java
@@ -1,20 +1,32 @@
 package com.example.hello;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 import am.ik.accesslogger.AccessLogger;
 import am.ik.accesslogger.AccessLoggerBuilder;
 
+import org.springframework.boot.actuate.web.exchanges.HttpExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.CollectionUtils;
 
 @Configuration(proxyBeanMethods = false)
 public class AppConfig {
 
 	@Bean
 	public AccessLogger accessLogger() {
-		return AccessLoggerBuilder.accessLogger().filter(httpExchange -> {
+		Predicate<HttpExchange> excludeActuator = httpExchange -> {
 			String uri = httpExchange.getRequest().getUri().getPath();
 			return uri != null && !(uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator"));
-		}).build();
+		};
+		Predicate<HttpExchange> excludeRoute53 = httpExchange -> {
+			List<String> userAgents = httpExchange.getRequest().getHeaders().get(HttpHeaders.USER_AGENT);
+			return !CollectionUtils.isEmpty(userAgents)
+					&& userAgents.getFirst().startsWith("Amazon-Route53-Health-Check-Service");
+		};
+		return AccessLoggerBuilder.accessLogger().filter(excludeRoute53.or(excludeActuator)).build();
 	}
 
 }
